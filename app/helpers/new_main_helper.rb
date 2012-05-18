d394271acd4febe9ed2eb8c22533187ae21fd8ed
Ravindra Kumar Kumawat <ravikumawat87@gmail.com>
2012-05-18 18:33:04 +0530

    two new file added

---
+++ app/helpers/new_main_helper.rb
@@ -0,0 +1,9 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+def new_helper
+  puts "Hello World"
+end
+
+def new_helper_method
+  puts "Hello World"
+end
