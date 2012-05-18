32396e9d82c2ca1ffab83afaedd18637d93352b9
Ravindra Kumar Kumawat <ravikumawat87@gmail.com>
2012-05-18 17:48:30 +0530

    add commit method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+
+  def hello
+     puts "new comants in github"
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def commant_method
+    puts "this is a practice"
+  end
 end
