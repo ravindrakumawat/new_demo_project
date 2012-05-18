d77f74439a5a2f69b1ebfa8569a01e303246892f
Ravindra Kumar Kumawat <ravikumawat87@gmail.com>
2012-05-18 18:49:24 +0530

    rename_file

--- app/helpers/new_main_helper.rb
+++
@@ -1,9 +0,0 @@
-# To change this template, choose Tools | Templates
-# and open the template in the editor.
-def new_helper
-  puts "Hello World"
-end
-
-def new_helper_method
-  puts "Hello World"
-end
